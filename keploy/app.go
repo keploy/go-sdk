0776439c2538cb30256b1ed605d556ed0ec1eab6
slayerjain <shubhamkjain@outlook.com>
2021-12-24 16:02:12 +0530

    add support for run events

--- keploy/app.go
+++ keploy/app.go
@@ -3,6 +3,7 @@ package keploy
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"go.uber.org/zap"
 	"io"
@@ -54,15 +55,63 @@ func (a *App) Test() {
 	// fetch test cases from web server and save to memory
 	time.Sleep(time.Second * 5)
 	tcs := a.fetch()
-	// call the service for each test case
-	for _, tc := range tcs {
-		fmt.Println("testing: ", tc.ID)
-		fmt.Println("testcase result: ", a.check(tc))
+	total := len(tcs)
+
+	// start a test run
+	id, err := a.start(total)
+	if err != nil {
+		a.Log.Error("failed to start test run", zap.Error(err))
+		return
 	}
-	//
+
+	a.Log.Info("starting test execution", zap.String("id", id), zap.Int("total tests", total))
+	passed := true
+	// call the service for each test case
+	for i, tc := range tcs {
+		a.Log.Info(fmt.Sprintf("testing %d of %d", i, total), zap.String("testcase id", tc.ID))
+		ok := a.check(id, tc)
+		if !ok {
+			passed = false
+		}
+		a.Log.Info("result", zap.Bool("passed", ok))
+	}
+
+	// end the test run
+	err = a.end(id, passed)
+	if err != nil {
+		a.Log.Error("failed to end test run", zap.Error(err))
+		return
+	}
+	a.Log.Info("test run completed", zap.String("run id", id), zap.Bool("passed overall", passed))
+
 }
 
-func (a *App) simulate(tc TestCase) (http.Header, []byte, error) {
+func (a *App) start(total int) (string, error) {
+	url := fmt.Sprintf("%s/regression/start?app=%s&total=%d", a.KeployHost, a.Name, total)
+	body, err := a.newGet(url)
+	if err != nil {
+		return "", err
+	}
+	var resp map[string]string
+
+	err = json.Unmarshal(body, &resp)
+	if err != nil {
+		return "", err
+	}
+
+	return resp["id"], nil
+}
+
+func (a *App) end(id string, status bool) error {
+	url := fmt.Sprintf("%s/regression/end?status=%t&id=%s", a.KeployHost, status, id)
+	_, err := a.newGet(url)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (a *App) simulate(tc TestCase) (*HttpResp, error) {
 	req, err := http.NewRequest(string(tc.HttpReq.Method), "http://"+a.Host+":"+a.Port+tc.URI, bytes.NewBufferString(tc.HttpReq.Body))
 	if err != nil {
 		panic(err)
@@ -74,32 +123,36 @@ func (a *App) simulate(tc TestCase) (http.Header, []byte, error) {
 
 	resp, err := a.client.Do(req)
 	if err != nil {
-		a.Log.Error("failed sending testcase request to backend", zap.Error(err))
-		return nil, nil, err
+		a.Log.Error("failed sending testcase request to app", zap.Error(err))
+		return nil, err
 	}
 
 	defer resp.Body.Close()
 
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		a.Log.Error("failed getting testcases from backend", zap.Error(err))
-		return nil, nil, err
+		a.Log.Error("failed reading simulated response from app", zap.Error(err))
+		return nil, err
 	}
-	return resp.Header, body, nil
+	return &HttpResp{
+		StatusCode: resp.StatusCode,
+		Header:     resp.Header,
+		Body:       string(body),
+	}, nil
 }
 
-func (a *App) check(tc TestCase) bool {
-	headers, body, err := a.simulate(tc)
+func (a *App) check(runId string, tc TestCase) bool {
+	resp, err := a.simulate(tc)
 	if err != nil {
 		a.Log.Error("failed to simulate request on local server", zap.Error(err))
 		return false
 	}
 
-	bin, err := json.Marshal(&DeNoiseReq{
-		ID:      tc.ID,
-		AppID:   a.Name,
-		Body:    string(body),
-		Headers: headers,
+	bin, err := json.Marshal(&TestReq{
+		ID:    tc.ID,
+		AppID: a.Name,
+		RunID: runId,
+		Resp:  *resp,
 	})
 	if err != nil {
 		a.Log.Error("failed to marshal testcase request", zap.String("url", tc.URI), zap.Error(err))
@@ -116,13 +169,13 @@ func (a *App) check(tc TestCase) bool {
 	r.Header.Set("key", a.LicenseKey)
 	r.Header.Set("Content-Type", "application/json")
 
-	resp, err := a.client.Do(r)
+	resp2, err := a.client.Do(r)
 	if err != nil {
 		a.Log.Error("failed to send test request to backend", zap.String("url", tc.URI), zap.Error(err))
 		return false
 	}
 	var res map[string]bool
-	b, err := ioutil.ReadAll(resp.Body)
+	b, err := ioutil.ReadAll(resp2.Body)
 	if err != nil {
 		a.Log.Error("failed to read response from backend", zap.String("url", tc.URI), zap.Error(err))
 	}
@@ -181,7 +234,7 @@ func (a *App) put(tcs TestCaseReq) {
 		a.Deps[id] = tcs.Deps
 		defer delete(a.Deps, id)
 
-		h, b, err := a.simulate(TestCase{
+		resp2, err := a.simulate(TestCase{
 			ID:       id,
 			Captured: tcs.Captured,
 			URI:      tcs.URI,
@@ -193,12 +246,12 @@ func (a *App) put(tcs TestCaseReq) {
 			return
 		}
 
-		bin2, err := json.Marshal(&DeNoiseReq{
-			ID:      res["id"],
-			AppID:   a.Name,
-			Body:    string(b),
-			Headers: h,
+		bin2, err := json.Marshal(&TestReq{
+			ID:    res["id"],
+			AppID: a.Name,
+			Resp:  *resp2,
 		})
+
 		if err != nil {
 			a.Log.Error("failed to marshall testcase request", zap.String("url", tcs.URI), zap.Error(err))
 			return
@@ -251,6 +304,9 @@ func (a *App) newGet(url string) ([]byte, error) {
 	resp, err := a.client.Do(req)
 	if err != nil {
 		return nil, err
+	}
+	if resp.StatusCode != http.StatusOK {
+		return nil, errors.New("failed to send get request: " + resp.Status)
 	}
 
 	defer resp.Body.Close()
