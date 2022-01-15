162508864381d18ad5685bc45d3ef4963a545a96
Ritik Jain <60597329+re-Tick@users.noreply.github.com>
2022-01-16 01:53:28 +0530

    Update mdb_test.go

--- integrations/mdb_test.go
+++ integrations/mdb_test.go
@@ -26,7 +26,7 @@ type Trainer struct {
 }
 
 func connect() *MongoDB{
-	clientOptions := options.Client().ApplyURI("mongodb+srv://admin:zYLnsfk29c770IE1@keploy-test.mujuh.mongodb.net/test?retryWrites=true&w=majority")
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
 	client, err := mongo.Connect(context.TODO(), clientOptions)
 	if err != nil {
 		log.Fatal(err)
