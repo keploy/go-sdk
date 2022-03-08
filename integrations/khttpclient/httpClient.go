e427a5ccd667bba25009898f1698cd0393e715fc
re-Tick <jain.ritik.1001@gmail.com>
2022-03-08 17:22:07 +0530

    fix(khttpClient): adds an interceptor for http client to mock the outputs of external http calls

--- integrations/khttpclient/httpClient.go
+++ integrations/khttpclient/httpClient.go
@@ -3,57 +3,19 @@ package khttpclient
 import (
 	"bytes"
 	"context"
+	"strconv"
+
 	"go.keploy.io/server/pkg/models"
 
-	// "crypto/tls"
 	"encoding/gob"
-	// "encoding/json"
 	"errors"
 	"fmt"
 	"io"
 	"io/ioutil"
 	"net/http"
-	"net/url"
-
-	// "strings"
-
 	"github.com/keploy/go-sdk/keploy"
 	"go.uber.org/zap"
 )
-
-// NewHttpClient is used to embed the http.Client pointer in order to mock its methods.
-// The purpose is to capture or replay its method's outputs according to "KEPLOY_SDK_MODE".
-// It returns nil if client parameter is nil.
-//
-//  Note: Always call SetCtxHttpClient method of *integrations.HttpClient before using http-client's method. This should be done so that
-//        request's context can be modified for storing or retrieving outputs of http methods.
-func NewHttpClient(client *http.Client) *HttpClient {
-	if client == nil {
-		return nil
-	}
-	gob.Register(ReadCloser{})
-	logger, _ := zap.NewProduction()
-	defer func() {
-		_ = logger.Sync() // flushes buffer, if any
-	}()
-	return &HttpClient{Client: client, log: logger}
-}
-
-// HttpClient used to mock http.Client methods in order to store or retrieve outputs from request's context.
-type HttpClient struct {
-	*http.Client
-	log *zap.Logger
-	ctx context.Context
-}
-
-// SetCtxHttpClient is used to set integrations.HttpClient.ctx to http.Request.Context().
-// It should be called before calling any http.Client method so that, their
-// outputs can be stored or retrieved from http.Request.Context() according to "KEPLOY_SDK_MODE".
-//
-// ctx parameter should be the context of http.Request.
-func (cl *HttpClient) SetCtxHttpClient(ctx context.Context) {
-	cl.ctx = ctx
-}
 
 // ReadCloser is used so that gob could encode-decode http.Response.
 type ReadCloser struct {
@@ -80,181 +42,79 @@ func (rc *ReadCloser) MarshalBinary() ([]byte, error) {
 	return nil, nil
 }
 
-func requestString(req *http.Request) string {
-	return fmt.Sprint("Method: ", req.Method, ", URL: ", req.URL, ", Proto: ", req.Proto, ", ProtoMajor: ", req.ProtoMajor, ", ProtoMinor: ", req.ProtoMinor, ", Header: ", req.Header, ", Body: ", req.Body, ", ContentLength: ", req.ContentLength, ", TransferEncoding: ", req.TransferEncoding, ", Close: ", req.Close, ", Host: ", req.Host, ", Form: ", req.Form, ", PostForm: ", req.PostForm, ", MultipartForm: ", req.MultipartForm, ", Trailer: ", req.Trailer, ", RemoteAddr: ", req.RemoteAddr, ", RequestURI: ", req.RequestURI, ", TLS: ", req.TLS, ", Response: ", req.Response, ", Context: ", req.Context())
+
+type Interceptor struct{
+	core http.RoundTripper
+	log *zap.Logger
+	kctx *keploy.Context
 }
 
-// Do is used to override http.Client's Do method. More about this net/http method: https://pkg.go.dev/net/http#Client.Do.
-func (cl *HttpClient) Do(req *http.Request) (*http.Response, error) {
-	if keploy.GetModeFromContext(cl.ctx) == keploy.MODE_OFF {
-		resp, err := cl.Client.Do(req)
-		return resp, err
-	}
-	var (
-		err  error
-		kerr *keploy.KError = &keploy.KError{}
-		resp *http.Response = &http.Response{}
-	)
-	kctx, er := keploy.GetState(cl.ctx)
-	if er != nil {
-		return nil, er
-	}
-	mode := kctx.Mode
-	body := requestString(req)
-	meta := map[string]string{
-		"name":      "http-client",
-		"type":      string(models.HttpClient),
-		"operation": "Do",
-		"Request":   body,
-	}
-	switch mode {
-	case "test":
-		//don't call http.Client.Do method
-	case "capture":
-		resp, err = cl.Client.Do(req)
-	default:
-		return nil, errors.New("integrations: Not in a valid sdk mode")
-	}
+// NewInterceptor constructs and returns the pointer to Interceptor. Interceptor is used
+// to intercept every http client calls and store their responses into keploy context.
+func NewInterceptor (core http.RoundTripper) *Interceptor{
+	// Initialize a logger
+	logger, _ := zap.NewProduction()
+	defer func() {
+		_ = logger.Sync() // flushes buffer, if any
+	}()
 
-	if err != nil {
-		kerr = &keploy.KError{Err: err}
-	}
+	// Register the ReadCloser type to gob encoder
+	gob.Register(ReadCloser{})
 
-	resp.Body = &ReadCloser{Body: resp.Body}
-	if resp.Request != nil {
-		resp.Request.Body = &ReadCloser{Body: resp.Request.Body}
+	return &Interceptor{
+		core: core,
+		log: logger,
 	}
+}
 
-	mock, res := keploy.ProcessDep(cl.ctx, cl.log, meta, resp, kerr)
-	if mock {
-		var mockErr error
-		x := res[1].(*keploy.KError)
-		if x.Err != nil {
-			mockErr = x.Err
+// SetContext is used to store the keploy context from request context into the Interceptor
+// kctx field.
+func (i *Interceptor) SetContext(requestContext context.Context) {
+	// ctx := context.TODO()
+	if kctx,err := keploy.GetState(requestContext); err==nil{
+		i.kctx = kctx
+		i.log.Debug("http client keploy interceptor's context has been set to : ", zap.Any("keploy.Context ", i.kctx))
+	}
+} 
+
+// setRequestContext returns the context with keploy context as value. It is called only 
+// when kctx field of Interceptor is not null. 
+func (i *Interceptor) setRequestContext(ctx context.Context) context.Context{
+	rctx := context.WithValue(ctx, keploy.KCTX, i.kctx)
+	return rctx
+}
+
+// RoundTrip is the custom method which is called before making http client calls to
+// capture or replay the outputs of external http service.
+func (i Interceptor) RoundTrip(r *http.Request) (*http.Response, error) {
+	
+	var reqBody []byte
+	if r.Body != nil { // Read
+		var err error
+		reqBody, err = ioutil.ReadAll(r.Body)
+		if err != nil {
+			// TODO right way to log errors
+			i.log.Error("Unable to read request body", zap.Error(err))
+			return nil,err
 		}
-		return resp, mockErr
 	}
-	return resp, err
-}
+	r.Body = ioutil.NopCloser(bytes.NewBuffer(reqBody)) // Reset
 
-// Get mocks the http.Client.Get method of net/http package. More about this net/http method: https://pkg.go.dev/net/http#Client.Get.
-func (cl *HttpClient) Get(url string) (*http.Response, error) {
-	if keploy.GetModeFromContext(cl.ctx) == keploy.MODE_OFF {
-		resp, err := cl.Client.Get(url)
-		return resp, err
+	// adds the keploy context stored in Interceptor's ctx field into the http client request context.
+	if i.kctx!=nil{
+		ctx := i.setRequestContext(r.Context())
+		r  = r.WithContext(ctx)
+	}
+	
+	if keploy.GetModeFromContext(r.Context()) == keploy.MODE_OFF {
+		return i.core.RoundTrip(r)
 	}
 	var (
 		err  error
 		kerr *keploy.KError = &keploy.KError{}
 		resp *http.Response = &http.Response{}
 	)
-	kctx, er := keploy.GetState(cl.ctx)
-	if er != nil {
-		return nil, er
-	}
-	mode := kctx.Mode
-	meta := map[string]string{
-		"name":      "http-client",
-		"type":      string(models.HttpClient),
-		"operation": "Get",
-		"URL":       url,
-	}
-	switch mode {
-	case "test":
-		//don't call http.Client.Get method
-	case "capture":
-		resp, err = cl.Client.Get(url)
-	default:
-		return nil, errors.New("integrations: Not in a valid sdk mode")
-	}
-
-	if err != nil {
-		kerr = &keploy.KError{Err: err}
-	}
-
-	resp.Body = &ReadCloser{Body: resp.Body}
-	if resp.Request != nil {
-		resp.Request.Body = &ReadCloser{Body: resp.Request.Body}
-	}
-
-	mock, res := keploy.ProcessDep(cl.ctx, cl.log, meta, resp, kerr)
-	if mock {
-		var mockErr error
-		x := res[1].(*keploy.KError)
-		if x.Err != nil {
-			mockErr = x.Err
-		}
-		return resp, mockErr
-	}
-	return resp, err
-}
-
-// Post mocks the http.Client.Post method. More about this net/http method: https://pkg.go.dev/net/http#Client.Post.
-func (cl *HttpClient) Post(url, contentType string, body io.Reader) (*http.Response, error) {
-	if keploy.GetModeFromContext(cl.ctx) == keploy.MODE_OFF {
-		resp, err := cl.Client.Post(url, contentType, body)
-		return resp, err
-	}
-	var (
-		err  error
-		kerr *keploy.KError = &keploy.KError{}
-		resp *http.Response = &http.Response{}
-	)
-	kctx, er := keploy.GetState(cl.ctx)
-	if er != nil {
-		return nil, er
-	}
-	mode := kctx.Mode
-	meta := map[string]string{
-		"name":        "http-client",
-		"type":        string(models.HttpClient),
-		"operation":   "Post",
-		"URL":         url,
-		"ContentType": contentType,
-		"body":        fmt.Sprint(body),
-	}
-	switch mode {
-	case "test":
-		//don't call http.Client.Post method
-	case "capture":
-		resp, err = cl.Client.Post(url, contentType, body)
-	default:
-		return nil, errors.New("integrations: Not in a valid sdk mode")
-	}
-
-	if err != nil {
-		kerr = &keploy.KError{Err: err}
-	}
-
-	resp.Body = &ReadCloser{Body: resp.Body}
-	if resp.Request != nil {
-		resp.Request.Body = &ReadCloser{Body: resp.Request.Body}
-	}
-
-	mock, res := keploy.ProcessDep(cl.ctx, cl.log, meta, resp, kerr)
-	if mock {
-		var mockErr error
-		x := res[1].(*keploy.KError)
-		if x.Err != nil {
-			mockErr = x.Err
-		}
-		return resp, mockErr
-	}
-	return resp, err
-}
-
-// Head mocks http.Client.Head method. More about this net/http method: https://pkg.go.dev/net/http#Client.Head.
-func (cl *HttpClient) Head(url string) (*http.Response, error) {
-	if keploy.GetModeFromContext(cl.ctx) == keploy.MODE_OFF {
-		resp, err := cl.Client.Head(url)
-		return resp, err
-	}
-	var (
-		err  error
-		kerr *keploy.KError = &keploy.KError{}
-		resp *http.Response = &http.Response{}
-	)
-	kctx, er := keploy.GetState(cl.ctx)
+	kctx, er := keploy.GetState(r.Context())
 	if er != nil {
 		return nil, er
 	}
@@ -262,67 +122,20 @@ func (cl *HttpClient) Head(url string) (*http.Response, error) {
 	meta := map[string]string{
 		"name":      "http-client",
 		"type":      string(models.HttpClient),
-		"operation": "Head",
-		"URL":       url,
-	}
-	switch mode {
-	case "test":
-		//don't call http.Client.Head method
-	case "capture":
-		resp, err = cl.Client.Head(url)
-	default:
-		return nil, errors.New("integrations: Not in a valid sdk mode")
-	}
-
-	if err != nil {
-		kerr = &keploy.KError{Err: err}
-	}
-
-	resp.Body = &ReadCloser{Body: resp.Body}
-	if resp.Request != nil {
-		resp.Request.Body = &ReadCloser{Body: resp.Request.Body}
-	}
-
-	mock, res := keploy.ProcessDep(cl.ctx, cl.log, meta, resp, kerr)
-	if mock {
-		var mockErr error
-		x := res[1].(*keploy.KError)
-		if x.Err != nil {
-			mockErr = x.Err
-		}
-		return resp, mockErr
-	}
-	return resp, err
-}
-
-// PostForm Method mocks net/http Client's method. About mocked method: https://pkg.go.dev/net/http#Client.PostForm.
-func (cl *HttpClient) PostForm(url string, data url.Values) (*http.Response, error) {
-	if keploy.GetModeFromContext(cl.ctx) == keploy.MODE_OFF {
-		resp, err := cl.Client.PostForm(url, data)
-		return resp, err
-	}
-	var (
-		err  error
-		kerr *keploy.KError = &keploy.KError{}
-		resp *http.Response = &http.Response{}
-	)
-	kctx, er := keploy.GetState(cl.ctx)
-	if er != nil {
-		return nil, er
-	}
-	mode := kctx.Mode
-	meta := map[string]string{
-		"name":      "http-client",
-		"type":      string(models.HttpClient),
-		"operation": "PostForm",
-		"URL":       url,
-		"Data":      fmt.Sprint(data),
+		"operation": r.Method,
+		"URL":       r.URL.String(),
+		"Header":    fmt.Sprint(r.Header),
+		"Body":      string(reqBody),
+		"Proto":     r.Proto,
+		"ProtoMajor":strconv.Itoa(r.ProtoMajor),
+		"ProtoMinor":strconv.Itoa(r.ProtoMinor),
 	}
 	switch mode {
 	case "test":
 		//don't call http.Client.PostForm method
 	case "capture":
-		resp, err = cl.Client.PostForm(url, data)
+		// resp, err = cl.Client.PostForm(url, data)
+		resp, err = i.core.RoundTrip(r)
 	default:
 		return nil, errors.New("integrations: Not in a valid sdk mode")
 	}
@@ -336,7 +149,7 @@ func (cl *HttpClient) PostForm(url string, data url.Values) (*http.Response, err
 		resp.Request.Body = &ReadCloser{Body: resp.Request.Body}
 	}
 
-	mock, res := keploy.ProcessDep(cl.ctx, cl.log, meta, resp, kerr)
+	mock, res := keploy.ProcessDep(r.Context(), i.log, meta, resp, kerr)
 	if mock {
 		var mockErr error
 		x := res[1].(*keploy.KError)
@@ -346,4 +159,5 @@ func (cl *HttpClient) PostForm(url string, data url.Values) (*http.Response, err
 		return resp, mockErr
 	}
 	return resp, err
-}
+
+  }
