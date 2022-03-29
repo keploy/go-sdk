13e7208d44c057d84e762714cc534de09fb34d87
Ritik Jain <60597329+re-Tick@users.noreply.github.com>
2022-03-29 09:25:05 +0530

    Update context.go

--- keploy/context.go
+++ keploy/context.go
@@ -1,19 +1,9 @@
 package keploy
 
-import (
-	// "time"
-
-	// "github.com/golang/protobuf/ptypes/timestamp"
-	// "go.keploy.io/server/http/regression"
-	"time"
-
-	// "github.com/golang/protobuf/ptypes/timestamp"
-	"go.keploy.io/server/pkg/models"
-)
+import "go.keploy.io/server/pkg/models"
 
 type Context struct {
 	Mode    Mode
 	TestID  string
 	Deps    []models.Dependency
-	Capture time.Time 
 }
