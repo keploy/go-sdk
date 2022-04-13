25f9500d83a36bc60558b809277ec25c58ade729
Ritik Jain <60597329+re-Tick@users.noreply.github.com>
2022-04-13 17:12:12 +0530

    refactor: refactor router code by adding a generic method. (#75)
    * feat(mongo): Adds Distinct, CountDocuments and Aggregate methods to mock their outputs.
    
    * refactor: refactors the router code by adding a generic method
    
    ProcessRequest is called before the next middleware to store request body, mock ResponseWriter and set context mode.
    
    * Update README.md
    
    * refactor: refactors the router code by adding a generic method
    
    processrequest is called before the next middleware to store request body, mock http.responsewriter and set context mode.
    
    Co-authored-by: Ritik Jain <ritikjain@Ritiks-MacBook-Air.local>

--- integrations/kchi/chi-v5.go
+++ integrations/kchi/chi-v5.go
@@ -1,94 +1,44 @@
 package kchi
 
 import (
-	"bytes"
-	"context"
-	"io"
-	"io/ioutil"
 	"net/http"
 
 	"github.com/go-chi/chi"
 	"github.com/keploy/go-sdk/keploy"
 	"go.keploy.io/server/pkg/models"
-	"go.uber.org/zap"
-	// "google.golang.org/genproto/googleapis/cloud/aiplatform/v1/schema/predict/params"
 )
 
-func ChiV5(k *keploy.Keploy, w *chi.Mux) {
-	if keploy.GetMode() == keploy.MODE_OFF {
-		return
-	}
-	w.Use(mw(k))
-}
-
-func captureRespChi(w http.ResponseWriter, r *http.Request, next http.Handler) models.HttpResp {
-	resBody := new(bytes.Buffer)
-	mw := io.MultiWriter(w, resBody)
-	writer := &keploy.BodyDumpResponseWriter{
-		Writer:         mw,
-		ResponseWriter: w,
-		Status:         http.StatusOK,
-	}
-	w = writer
-
-	next.ServeHTTP(w, r)
-	return models.HttpResp{
-		//Status
-
-		StatusCode: writer.Status,
-		Header:     w.Header(),
-		Body:       resBody.String(),
-	}
-}
-
-func mw(k *keploy.Keploy) func(http.Handler) http.Handler {
-	if k == nil {
+func ChiMiddlewareV5(k *keploy.Keploy) func(http.Handler) http.Handler {
+	if k == nil || keploy.GetMode() == keploy.MODE_OFF {
 		return func(next http.Handler) http.Handler {
 			return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-				k.Log.Warn("keploy is nil.")
 				next.ServeHTTP(w, r)
 			})
 		}
 	}
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			writer, r, resBody, reqBody, err := keploy.ProcessRequest(w, r, k)
+			if err != nil {
+				return
+			}
+			w = writer
+
+			// Store the responses
+			next.ServeHTTP(w, r)
+			resp := models.HttpResp{
+				//Status
+				StatusCode: writer.Status,
+				Header:     w.Header(),
+				Body:       resBody.String(),
+			}
+
 			id := r.Header.Get("KEPLOY_TEST_ID")
 			if id != "" {
-				// id is only present during simulation
-				// run it similar to how testcases would run
-				ctx := context.WithValue(r.Context(), keploy.KCTX, &keploy.Context{
-					Mode:   "test",
-					TestID: id,
-					Deps:   k.GetDependencies(id),
-				})
-
-				r = r.WithContext(ctx)
-				resp := captureRespChi(w, r, next)
 				k.PutResp(id, resp)
 				return
 			}
-			ctx := context.WithValue(r.Context(), keploy.KCTX, &keploy.Context{
-				Mode: "capture",
-			})
 
-			r = r.WithContext(ctx)
-
-			// Request
-			var reqBody []byte
-			var err error
-			if r.Body != nil { // Read
-				reqBody, err = ioutil.ReadAll(r.Body)
-				if err != nil {
-					// TODO right way to log errors
-					k.Log.Error("Unable to read request body", zap.Error(err))
-					return
-				}
-			}
-			r.Body = ioutil.NopCloser(bytes.NewBuffer(reqBody)) // Reset
-
-			resp := captureRespChi(w, r, next)
-			// params := webgo.Context(r).Params()
-			// params := chi.RouteContext(r.Context()).URLParams
 			params := urlParamsChi(chi.RouteContext(r.Context()), k)
 			keploy.CaptureTestcase(k, r, reqBody, resp, params)
 		})
--- integrations/kmux/mux.go
+++ integrations/kmux/mux.go
@@ -1,97 +1,50 @@
 package kmux
 
 import (
-	"bytes"
-	"context"
-	"io"
-	"io/ioutil"
 	"net/http"
 
 	"github.com/gorilla/mux"
 	"github.com/keploy/go-sdk/keploy"
 	"go.keploy.io/server/pkg/models"
-	"go.uber.org/zap"
 )
 
-// Mux adds keploy instrumentation for Mux router.
+// MuxMiddleware adds keploy instrumentation for Mux router.
 // It should be ideally used after other instrumentation libraries like APMs.
 //
 // k is the Keploy instance
-//
-// w is the mux router instance
-func Mux(k *keploy.Keploy, w *mux.Router) {
-	if keploy.GetMode() == keploy.MODE_OFF {
-		return
-	}
-	w.Use(mw(k))
-}
-
-func captureRespMux(w http.ResponseWriter, r *http.Request, next http.Handler) models.HttpResp {
-	resBody := new(bytes.Buffer)
-	mw := io.MultiWriter(w, resBody)
-	writer := &keploy.BodyDumpResponseWriter{
-		Writer:         mw,
-		ResponseWriter: w,
-		Status:         http.StatusOK,
-	}
-	w = writer
-
-	next.ServeHTTP(w, r)
-	return models.HttpResp{
-		//Status
-
-		StatusCode: writer.Status,
-		Header:     w.Header(),
-		Body:       resBody.String(),
-	}
-}
-
-func mw(k *keploy.Keploy) func( http.Handler) http.Handler {
-	if k == nil {
-		return func(next http.Handler) http.Handler{
-			return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request)  {
+func MuxMiddleware(k *keploy.Keploy) func(http.Handler) http.Handler {
+	if k == nil || keploy.GetMode() == keploy.MODE_OFF {
+		return func(next http.Handler) http.Handler {
+			return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 				next.ServeHTTP(w, r)
 			})
 		}
 	}
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			writer, r, resBody, reqBody, err := keploy.ProcessRequest(w, r, k)
+			if err != nil {
+				return
+			}
+			w = writer
+
+			// Store the responses
+			next.ServeHTTP(w, r)
+			resp := models.HttpResp{
+				//Status
+				StatusCode: writer.Status,
+				Header:     w.Header(),
+				Body:       resBody.String(),
+			}
+
 			id := r.Header.Get("KEPLOY_TEST_ID")
 			if id != "" {
 				// id is only present during simulation
 				// run it similar to how testcases would run
-				ctx := context.WithValue(r.Context(), keploy.KCTX, &keploy.Context{
-					Mode:   "test",
-					TestID: id,
-					Deps:   k.GetDependencies(id),
-				})
-
-				r = r.WithContext(ctx)
-				resp := captureRespMux(w, r, next)
 				k.PutResp(id, resp)
 				return
 			}
-			ctx := context.WithValue(r.Context(), keploy.KCTX, &keploy.Context{
-				Mode: "capture",
-			})
 
-			r = r.WithContext(ctx)
-
-			// Request
-			var reqBody []byte
-			var err error
-			if r.Body != nil { // Read
-				reqBody, err = ioutil.ReadAll(r.Body)
-				if err != nil {
-					// TODO right way to log errors
-					k.Log.Error("Unable to read request body", zap.Error(err))
-					return
-				}
-			}
-			r.Body = ioutil.NopCloser(bytes.NewBuffer(reqBody)) // Reset
-
-			resp := captureRespMux(w, r, next)
-			// params := webgo.Context(r).Params()
 			params := mux.Vars(r)
 			keploy.CaptureTestcase(k, r, reqBody, resp, params)
 
