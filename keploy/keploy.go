2cfbadeb6ee686e366a02e093c4be0d5eb48dc06
Pranshu Srivastava <37413698+PranshuSrivastava@users.noreply.github.com>
2023-01-06 18:56:40 +0530

    chore: use uniform default test and mock folders (#159)
    * changed the file structure made by the go sdk
    
    Signed-off-by: Pranshu Srivastava <iampranshu24@gmail.com>
    
    * changed the names of the files
    
    Signed-off-by: Pranshu Srivastava <iampranshu24@gmail.com>
    
    Signed-off-by: Pranshu Srivastava <iampranshu24@gmail.com>
    Co-authored-by: Shubham Jain <shubhamkjain@outlook.com>

--- keploy/keploy.go
+++ keploy/keploy.go
@@ -143,7 +143,7 @@ func New(cfg Config) *Keploy {
 		if err != nil {
 			logger.Error("Failed to get the path of current directory", zap.Error(err))
 		}
-		cfg.App.TestPath = path + "/keploy-tests"
+		cfg.App.TestPath = path + "/keploy/tests"
 	}
 	if len(cfg.App.MockPath) > 0 && cfg.App.MockPath[0] != '/' {
 		path, err := filepath.Abs(cfg.App.MockPath)
@@ -152,11 +152,11 @@ func New(cfg Config) *Keploy {
 		}
 		cfg.App.MockPath = path
 	} else if len(cfg.App.MockPath) == 0 {
-		// path, err := cfg.App.TestPath+"/mocks"
+		path, err := os.Getwd()
 		if cfg.App.TestPath == "" {
 			logger.Error("Failed to get the path of current directory", zap.Error(err))
 		}
-		cfg.App.MockPath = cfg.App.TestPath + "/mocks"
+		cfg.App.MockPath = path + "/keploy/mocks"
 	}
 
 	k := &Keploy{
