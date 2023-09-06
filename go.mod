7a64939ddc60ad17a2032dd190e3ce3c48046d2d
charankamarapu <kamarapucharan@gmail.com>
2023-09-06 16:59:54 +0000

    feat: upgrade to v2
    Signed-off-by: charankamarapu <kamarapucharan@gmail.com>

--- go.mod
+++ go.mod
@@ -1,37 +1,14 @@
-module github.com/keploy/go-sdk
+module github.com/keploy/go-sdk/v2
 
 go 1.16
 
 //replace go.keploy.io/server => ../keploy
 
-require (
-	github.com/aws/aws-sdk-go v1.42.23
-	github.com/bnkamalesh/webgo/v4 v4.1.11
-	github.com/bnkamalesh/webgo/v6 v6.2.2
-	github.com/gin-gonic/gin v1.8.1
-	github.com/go-playground/validator/v10 v10.10.1
-	github.com/labstack/echo/v4 v4.9.0
-	go.mongodb.org/mongo-driver v1.8.3
-	go.uber.org/zap v1.22.0
-	google.golang.org/grpc v1.48.0
-)
+require go.uber.org/zap v1.22.0
 
 require (
-	github.com/araddon/dateparse v0.0.0-20210429162001-6b43995a97de
-	github.com/benbjohnson/clock v1.1.0
-	github.com/go-chi/chi v1.5.4
-	github.com/go-redis/redis/v8 v8.11.5
-	github.com/go-test/deep v1.0.8
-	github.com/gorilla/mux v1.8.0
-	github.com/jhump/protoreflect v1.14.0
-	github.com/lestrrat-go/jwx v1.2.25
-	github.com/valyala/fasthttp v1.44.0
-)
-
-require (
-	github.com/creasty/defaults v1.6.0
-	github.com/fullstorydev/grpcurl v1.8.7
-	github.com/golang/protobuf v1.5.2
-	go.keploy.io/server v0.8.6
-	google.golang.org/protobuf v1.28.1
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/stretchr/testify v1.7.1 // indirect
+	go.uber.org/atomic v1.9.0 // indirect
+	go.uber.org/multierr v1.7.0 // indirect
 )
--- pkg/keploy/mode.go
+++ pkg/keploy/mode.go
@@ -1,31 +1,15 @@
 package keploy
 
-import (
-	"context"
-	"errors"
-	"sync"
-)
-
 // Mode represents the mode at which the SDK is operating
 // MODE_RECORD is for recording API calls to generate testcases
 // MODE_TEST is for testing the application on previous recorded testcases
 // MODE_OFF disables keploy SDK automatically from the application
 type Mode string
 
-type KctxType string
-
-var mu sync.Mutex
-
 const (
-	MODE_RECORD Mode     = "record"
-	MODE_TEST   Mode     = "test"
-	MODE_OFF    Mode     = "off"
-	KCTX        KctxType = "KeployContext"
-	KTime       KctxType = "KeployTime"
-)
-
-var (
-	mode = MODE_OFF
+	MODE_RECORD Mode = "record"
+	MODE_TEST   Mode = "test"
+	MODE_OFF    Mode = "off"
 )
 
 // Valid checks if the provided mode is valid
@@ -34,44 +18,4 @@ func (m Mode) Valid() bool {
 		return true
 	}
 	return false
-}
-
-// GetMode returns the mode of the keploy SDK
-func GetMode() Mode {
-	return mode
-}
-
-// SetTestMode sets the keploy SDK mode to MODE_TEST
-func SetTestMode() {
-	_ = SetMode(MODE_TEST)
-}
-
-// SetMode sets the keploy SDK mode
-// error is returned if the mode is invalid
-func SetMode(m Mode) error {
-	mu.Lock()
-	if !m.Valid() {
-		return errors.New("invalid mode: " + string(m))
-	}
-	mode = m
-	mu.Unlock()
-	return nil
-}
-
-// GetModeFromContext returns the mode on which SDK is configured by accessing environment variable.
-func GetModeFromContext(ctx context.Context) Mode {
-	kctx, err := GetState(ctx)
-	if err != nil {
-		return MODE_OFF
-	}
-	return kctx.Mode
-}
-
-// GetState returns value of "KeployContext" key-value pair which is stored in the request context.
-func GetState(ctx context.Context) (*Context, error) {
-	kctx := ctx.Value(KCTX)
-	if kctx == nil {
-		return nil, errors.New("failed to get Keploy context")
-	}
-	return kctx.(*Context), nil
 }
